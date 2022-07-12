685696b729a4a10e7e57944155b4087b86b059a0
haitranduc4270 <hai.tranduc4270@gmail.com>
2022-07-12 21:15:18 +0700

    remove 404

--- app.js
+++ app.js
@@ -10,11 +10,11 @@ const app = require('./library/application');
 app.addRoute(BookRoute);
 app.addRoute(UserRoute);
 
-app.use(function errorHandler(req, res, next) {
-    res.statusCode = 400;
-    res.setHeader('Content-Type', 'application/json');
-    res.write('404 not found');
-    res.end();
-})
+// app.use(function errorHandler(req, res, next) {
+//     res.statusCode = 400;
+//     res.setHeader('Content-Type', 'application/json');
+//     res.write('404 not found');
+//     res.end();
+// })
 
 module.exports = app.submit;
