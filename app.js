0a27c7e37d58dffad1af8a9bb8e8a7051db50d14
haitranduc4270 <hai.tranduc4270@gmail.com>
2022-07-13 21:19:25 +0700

    Deploy main

--- app.js
+++ app.js
@@ -6,19 +6,19 @@ const db = require('./config/connect');
 db.connect();
 
 const app = require('./library/application');
-const cors = require('./library/cors.js');
+// const cors = require('./library/cors.js');
 
-// app.use(function(req, res, next) {
-//     res.setHeader('Access-Control-Allow-Origin', '*');
-//     res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-//     next(req, res, next);
-// });
+app.use(function(req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader('Access-Control-Allow-Credentials', 'true');
+    next(req, res, next);
+});
 
-const corsOptions = {
-    origin: '*',
-    credentials: true,            //access-control-allow-credentials:true
-    optionSuccessStatus: 200,
-}
+// const corsOptions = {
+//     origin: '*',
+//     credentials: true,            //access-control-allow-credentials:true
+//     optionSuccessStatus: 200,
+// }
 
 app.use(cors(corsOptions)) // Use this after the variable declaration
 
